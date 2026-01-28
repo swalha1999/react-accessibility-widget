05d29595f197eea02ede3238d16a62e19b58cde9
swalha1999 <mohamd514.m@gmail.com>
2026-01-28 21:18:41 +0200

    Auto-create .nojekyll after build

--- demo/vite.config.ts
+++ demo/vite.config.ts
@@ -1,8 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { writeFileSync } from 'fs';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      name: 'create-nojekyll',
+      closeBundle() {
+        writeFileSync('../docs/.nojekyll', '');
+      },
+    },
+  ],
   base: '/react-accessibility-widget/',
   build: {
     outDir: '../docs',
